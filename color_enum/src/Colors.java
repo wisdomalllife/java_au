d3ff69cef98e425788e782195a202b0fcf426e5b
VictoriaShumkova <wisdomalllife@gmail.com>
2020-12-19 14:36:36 +0300

    CREDIT-2022 Enums

---
+++ color_enum/src/Colors.java
@@ -0,0 +1,14 @@
+public class Colors {
+    public static void main(String[] args) {
+
+        ColorsOfPoints[] types = ColorsOfPoints.values();
+        for (ColorsOfPoints s : types) { System.out.println(s); }
+    }
+}
+
+
+enum ColorsOfPoints {
+    Black,
+    Grey,
+    White;
+}
