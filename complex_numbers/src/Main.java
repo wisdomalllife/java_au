dcef3f35490089d37373cabab40b828540c8c750
wisdomalllife <55911109+wisdomalllife@users.noreply.github.com>
2020-12-20 18:41:13 +0300

    Update Main.java

--- complex_numbers/src/Main.java
+++ complex_numbers/src/Main.java
@@ -1,12 +1,1 @@
-import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        if (args.length < 2) { // проверка что  мы получили текст и число
-            return;
-        }
-        if (isThereNumber(number)) {
-            complexCount(number, complexList);
-        }
-    }
-}
